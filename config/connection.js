87f8d56692b7f1c344ecb24b102e2ab6204c98be
Amanda Hoffman <amandah@alumni.rutgers.edu>
2016-08-07 21:44:12 -0400

    attempting deployment to heroku

--- config/connection.js
+++ config/connection.js
@@ -2,13 +2,19 @@
 Here is where you make the connection to the burger database and export and used by the O.R.M.
 */
 var mysql = require('mysql');
-var connection = mysql.createConnection({
-  port: 3306,
-  host: 'localhost',
-  user: 'unikitty',
-  password: 'glitterbomb',
-  database: 'burgers_db'
-});
+var connection;
+
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    port: 3306,
+    host: 'localhost',
+    user: 'unikitty',
+    password: 'glitterbomb',
+    database: 'burgers_db'
+  });
+};
 
 connection.connect(function (err) {
   if (err) {
